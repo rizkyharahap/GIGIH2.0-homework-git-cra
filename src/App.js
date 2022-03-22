4259a91638e3423e4f9a258dba5eca3877c4dc48
rizkyharahap <rizkyhh1999@gmail.com>
2022-03-22 07:56:52 +0700

    Add Single
    Create layout for single, and call single data from single.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import single from "./assets/data/single";
 
 function App() {
+  const { album, artists, name } = single;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      {/* Playlist card */}
+      <div className="card-playlist">
+        {/* Album cover  */}
+        <picture className="playlist-album">
+          <source
+            media={`(min-width:${album.images[1].width})`}
+            srcset={album.images[1].url}
+          />
+          <source
+            media={`(min-width:${album.images[2].width})`}
+            srcset={album.images[2].url}
+          />
+          <img src={album.images[0].url} alt={name} />
+        </picture>
+
+        {/* Description Album  */}
+        <div className="playlist-description">
+          <h2>{album.name}</h2>
+          <p>{artists[0].name}</p>
+
+          <button className="btn btn-playlist" title="Button">
+            Select
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
 
